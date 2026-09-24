9040266d74350343e6b75bcdb338491c35c7c4e9
Yuvraj Katiyar <dev.katiyaryuvraj@gmail.com>
2026-09-24 20:46:37 +0530

    updated carrerquiz

--- frontend/src/pages/carrerquiz.jsx
+++ frontend/src/pages/carrerquiz.jsx
@@ -199,7 +199,6 @@ export default function CareerQuizPage() {
         description: `Recommended learning path for ${answers[1]}.`,
 
         skills: skillsByCareer[answers[1]] || [],
-        
       });
       setShowResult(true);
       fetchRecommendedCourses(answers[1], answers[2]);
